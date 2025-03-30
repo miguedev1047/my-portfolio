73223801d910971440492bea1419735ac37c5232
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-29 22:17:48 -0500

    FEAT: add general const file ⚙️

---
+++ constants/general.ts
@@ -0,0 +1,42 @@
+import { GenshinDiaryProjectImg } from '@/assets/projetcs/_index'
+
+export const NAV_ITEMS = [
+  {
+    title: 'Inicio',
+    href: '#home',
+  },
+  {
+    title: 'Proyectos',
+    href: '#projects',
+  },
+  {
+    title: 'Acerca de mi',
+    href: '#about',
+  },
+]
+
+export const MY_TECHONOLOGIES = [
+  'React',
+  'NextJS',
+  'Turso',
+  'Prisma',
+  'Tailwind',
+  'Typescript',
+]
+
+export const PROJECTS_ITEMS = [
+  {
+    title: 'Genshin Diary',
+    description:
+      'Sitio web que ofrece guías sencillas para tus personajes favoritos de Genshin Impact.',
+    technologies: ['NextJS', 'React', 'Tailwind', 'Shadcn', 'Prisma', 'Turso'],
+    image: GenshinDiaryProjectImg.src,
+    href: 'https://genshin-diary.vercel.app/',
+    github: 'https://github.com/miguedev1047/genshin-diary-app',
+    isCode: true,
+    isSource: true,
+  },
+]
+
+export const EMAIL = 'miguelangeljimenez1047@gmail.com'
+export const GITHUB = 'https://github.com/miguedev1047'
