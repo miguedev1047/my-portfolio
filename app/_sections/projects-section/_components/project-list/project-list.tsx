47fbc515587beef74b5d9975b3c795dc2ccf9105
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-29 22:22:50 -0500

    FEAT: add project list component 🎨

---
+++ app/_sections/projects-section/_components/project-list/project-list.tsx
@@ -0,0 +1,14 @@
+import { PROJECTS_ITEMS } from '@/constants/general'
+import { ProjectItem } from '@/app/_sections/projects-section/_components/project-item'
+
+export function ProjectList() {
+  const MAPPED_PROJECTS = PROJECTS_ITEMS.map((item, index) => (
+    <ProjectItem key={index} {...item} />
+  ))
+
+  return (
+    <ul className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
+      {MAPPED_PROJECTS}
+    </ul>
+  )
+}
