e276e16bfdf47dccfd531da512cad9ad9095ac65
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-17 17:32:08 -0800

    Remove GenshinDiaryProjectImg export

--- assets/projetcs/_index.ts
+++ assets/projetcs/_index.ts
@@ -1,10 +1,8 @@
-import GenshinDiaryProjectImg from '@/assets/projetcs/genshin-diary-project.png'
 import IgCloneProjectImg from '@/assets/projetcs/ig-clone-project.png'
 import TrikiGameProjectImg from '@/assets/projetcs/triki-game-project.png'
 import CalendarAppProjectImg from '@/assets/projetcs/calendar-app-project.png'
 
 export {
-  GenshinDiaryProjectImg,
   IgCloneProjectImg,
   TrikiGameProjectImg,
   CalendarAppProjectImg,
